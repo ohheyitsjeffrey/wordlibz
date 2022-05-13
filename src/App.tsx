7ad9ebfb5738c49908c520ceba0b6060bf785812
jeffrey <jeffbenvenuti@gmail.com>
2022-05-13 23:13:15 +0000

    Add baseurl, remove redux counter boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-Header-Branding">
-        Wordlibz
-        </h1>
-        <p>
-        A word game inspired by mad libs.
-        </p>
-        <p>
-        Coming Soon!
-        </p>
+        <h1 className="App-Header-Branding">Wordlibz</h1>
+        <p>A word game inspired by mad libs.</p>
+        <p>Coming Soon!</p>
         <p>
           <a href="https://github.com/ohheyitsjeffrey/wordlibz">Github</a>
         </p>
       </header>
-      <div>
-      </div>
+      <div></div>
     </div>
   );
 }
---
+++ src/api/getRandomArticle.ts
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+export const getRandomArticle = () => {
+
+    return {};
+}
