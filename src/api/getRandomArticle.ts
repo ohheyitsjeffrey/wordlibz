effd55e985945b5eca49158c0853f66a6aa77119
jeffrey <jeffbenvenuti@gmail.com>
2022-05-15 01:04:25 +0000

    getRandomArticle

--- src/api/getRandomArticle.ts
+++ src/api/getRandomArticle.ts
@@ -1,7 +1,17 @@
 /**
- * Returns a random article in JSON format.
+ * Returns a promise which resolves to random article summary from Wikipedia in JSON format.  The url for 
+ * the request is baked in, so it requires no parameters.
+ * 
+ * @async
+ * @param none
  */
-export const getRandomArticle = () => {
+export const getRandomArticle = async ()  => {
 
-    return {};
+    // gives us a random summary from wikipedia.
+    const url = 'https://en.wikipedia.org/api/rest_v1/page/random/summary'
+
+    const article = await fetch(url);
+
+    return article.json();
 }
+
--- src/stories/api.stories.tsx
+++ src/stories/api.stories.tsx
@@ -1,8 +1,9 @@
 import { ComponentMeta } from '@storybook/react';
 import React, { useState } from 'react';
+import { getRandomArticle } from 'api/getRandomArticle';
 
 // A sloppy quick way to test this api function without fighting with typescript and REPL
-export const getArticleTestUI = () => {
+export const GetRandomArticleTestUI = () => {
   const defaultMessage = 'Response will appear here when it arrives.';
 
   const [responseMessage, setResponseMessage] = useState(defaultMessage);
@@ -12,7 +13,16 @@ export const getArticleTestUI = () => {
       <h1>getRandomArticle()</h1>
       <br />
       <br />
-      <button>Click To Test</button>
+      <button
+        onClick={async () => {
+          let article = await getRandomArticle().then((response) => {
+            setResponseMessage(JSON.stringify(response));
+            console.log(response);
+          });
+        }}
+      >
+        Click To Test
+      </button>
       <br />
       <br />
       <h2>Response</h2>
@@ -23,9 +33,9 @@ export const getArticleTestUI = () => {
 
 export default {
   title: 'API/getRandomArticle',
-  component: getArticleTestUI,
+  component: GetRandomArticleTestUI,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen'
   }
-} as ComponentMeta<typeof getArticleTestUI>;
+} as ComponentMeta<typeof GetRandomArticleTestUI>;
